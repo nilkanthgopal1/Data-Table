d798ff7cf892ba6666743e1741024925429feb4a
nilkanthgopal1 <nilkanthgopal1@gmail.com>
2022-07-26 22:35:08 +0530

    Data Table code

---
+++ src/main/java/com/example/Datatable/DatatableApplication.java
@@ -0,0 +1,13 @@
+package com.example.Datatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatableApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Datatable/Homecontroller.java
@@ -0,0 +1,14 @@
+package com.example.Datatable;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Homecontroller {
+
+	@RequestMapping("/")
+	public String indexPage()
+	{
+		return "index";
+	}
+}
